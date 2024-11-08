057b8d4d48a79e642f7a6c00013a3162c28404d7
sanhijazi <h.hijazi711@gmail.com>
2024-11-08 18:23:46 +0100

    Adding router and server folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useCallback, useEffect, useReducer } from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { getApiClient } from "./server/get_api_client";
+import { UserContext } from "./contexts/user_context";
 
-function App() {
-  return <div>Welcome Back</div>;
-}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "set-user":
+      return { ...state, user: action.me };
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    user: null,
+  });
+  const { user } = state;
+
+  useEffect(() => {
+    if (localStorage.getItem("unige-connect_token")) {
+      getApiClient()
+        .me()
+        .then((response) =>
+          dispatch({ type: "set-user", me: response.data.me })
+        );
+    }
+  }, []);
+
+  const setUser = useCallback((me) => {
+    dispatch({ type: "set-user", me: me });
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <RouterProvider router={router} />
+    </UserContext.Provider>
+  );
+};
 
 export default App;
---
+++ src/pages/page-not-found/page_not_found.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const PageNotFound = () => {
+  useEffect(() => {
+    document.title = "Page Not Found";
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div style={{ position: "relative" }}>
+      <FlexWrapper>
+        <Page404Title>404</Page404Title>
+        <PageNotFoundText as="p">
+          The page you&#x2019;re looking for can&#x2019;t be found.
+        </PageNotFoundText>
+        <FlexWrapper>
+          <LinkWrapper>
+            <CustomLink to="/">Go To Home</CustomLink>
+          </LinkWrapper>
+        </FlexWrapper>
+      </FlexWrapper>
+    </div>
+  );
+};
+
+const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Page404Title = styled.h1`
+  color: #131313;
+  font-size: 6em;
+  font-family: "Corbel Bold";
+`;
+
+const PageNotFoundText = styled.p`
+  color: #131313;
+  font-size: 1em;
+  font-family: "Corbel";
+  @media screen and (min-width: 768px) {
+    font-size: 1.2em;
+  }
+`;
+
+const LinkWrapper = styled.div`
+  padding: 8px;
+  margin-bottom: 40px;
+  font-family: "Corbel";
+  @media screen and (min-width: 360px) {
+    padding: 16px;
+  }
+`;
+
+const CustomLink = styled(Link)`
+  text-decoration: none !important;
+  font-weight: 600;
+  color: #2e607a;
+
+  :hover {
+    color: #2e607a;
+  }
+`;
+
+export default PageNotFound;
