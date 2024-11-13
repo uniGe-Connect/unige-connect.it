85694df4ba7aee0cc3ccc34129c591f246a92d9f
keat <kavano.aresia@gmail.com>
2024-11-13 17:47:20 +0100

    clean some files eslint

--- src/common/heading.js
+++ src/common/heading.js
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
-import { Header } from "semantic-ui-react";
+import { Header } from 'semantic-ui-react';
 
 const Heading = () => {
-
   return (
     <Title> UniGe Connect </Title>
-  )
+  );
 };
 
 const Title = styled(Header).attrs({
   as: 'h1',
 })`
-  font-family: "Roboto Slab" !important;
+  font-family: 'Roboto Slab' !important;
   color: var(--blue) !important;
 `;
 
--- src/common/nav.js
+++ src/common/nav.js
@@ -1,36 +1,24 @@
 import React, { useState } from 'react';
-import { MenuItem, Menu } from 'semantic-ui-react'
+import { MenuItem, Menu } from 'semantic-ui-react';
 
 const Nav = () => {
   const [activeItem, setActiveItem] = useState('home');
 
   return (
-    <Menu fluid widths={3} style={{ margin: '40px'}}>
-      <MenuItem 
-        name='home'
-        active={activeItem === 'home'}
-        onClick={() => setActiveItem('home')}
-      >
+    <Menu fluid widths={3} style={{ margin: '40px' }}>
+      <MenuItem name='home' active={activeItem === 'home'} onClick={() => setActiveItem('home')}>
         Home
       </MenuItem>
 
-      <MenuItem
-        name='your_groups'
-        active={activeItem === 'your_groups'}
-        onClick={() => setActiveItem('your_groups')}
-      >
+      <MenuItem name='your_groups' active={activeItem === 'your_groups'} onClick={() => setActiveItem('your_groups')}>
         Your groups
       </MenuItem>
 
-      <MenuItem
-        name='IDK'
-        active={activeItem === 'IDK'}
-        onClick={() => setActiveItem('IDK')}
-      >
+      <MenuItem name='IDK' active={activeItem === 'IDK'} onClick={() => setActiveItem('IDK')}>
         IDK
       </MenuItem>
     </Menu>
-  )
+  );
 };
 
 export default Nav;
