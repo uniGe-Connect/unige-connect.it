5ad95d25fc61fa0a5bfd44a60b195b473cb3e14f
keat <kavano.aresia@gmail.com>
2024-11-17 22:40:51 +0100

    fix merge

--- src/router.js
+++ src/router.js
@@ -1,14 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-// Pages
-import Home from './pages/home/home';
-import PageNotFound from './pages/page-not-found/page_not_found';
+//Pages
+import Home from "./pages/home/home";
+import PageNotFound from "./pages/page-not-found/page_not_found";
+import YourGroups from "./pages/your-groups/your_groups";
 
 export const router = createBrowserRouter(
   [
     {
       path: '/',
       element: <Home />,
+      errorElement: <PageNotFound />,
+    },
+    {
+      path: "/your-groups",
+      element: <YourGroups />,
       errorElement: <PageNotFound />,
     },
   ],
