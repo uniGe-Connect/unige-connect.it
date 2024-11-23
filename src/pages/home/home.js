bfde20bb7fc1b6bf8525b2cbf6a69825792c4911
sanhijazi <h.hijazi711@gmail.com>
2024-11-23 19:55:12 +0100

    add nav and footer to home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,11 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import Body from './body';
 import BottomPart from './bottomPart';
+import Nav from '../../common/nav';
+import Footer from '../../common/footer';
+
 function Home() {
   return (
   <Container>
+    <Nav />
     <Body />
     <BottomPart />
+    <Footer />
   </Container>);
 }
 
