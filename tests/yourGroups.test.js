ac6a6cd06610133b669558caef6f106ec0a564eb
keat <kavano.aresia@gmail.com>
2024-11-28 15:24:28 +0100

    solved previous tests

--- tests/yourGroups.test.js
+++ tests/yourGroups.test.js
@@ -1,15 +1,18 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import GroupsPage from '../src/pages/groups/your-groups/yourGroups';
+import { BrowserRouter } from 'react-router-dom'
 import '@testing-library/jest-dom/extend-expect';
 
 // Mocking the API calls
 jest.mock('../src/server/get_api_client', () => ({
   getApiClient: () => ({
-    createGroup: jest.fn().mockResolvedValue({ data: { id: 4 } }),
+    createGroup: jest.fn().mockResolvedValue({ data: { id: 90 } }),
     getOwnedGroups: jest.fn().mockResolvedValue({ data: [] }),
   }),
   makeStandardApiErrorHandler: jest.fn(),
 }));
+
+jest.mock('../src/permissions/RequireUserAccess', () => (Component) => (props) => <Component {...props} />);
 
 describe('GroupsPage', () => {
 
@@ -31,14 +34,20 @@ describe('GroupsPage', () => {
   });
 
   test('should create a new group and display a card', async () => {
-    render(<GroupsPage />);
+    render(<BrowserRouter><GroupsPage /></BrowserRouter>);
     const createGroupButton = screen.getByLabelText(/create-group-button/i);
     fireEvent.click(createGroupButton);
+    expect(screen.getByPlaceholderText(/group name/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/topic/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/description/i)).toBeInTheDocument();
+
     fireEvent.change(screen.getByPlaceholderText(/group name/i), { target: { value: 'New Group' } });
     fireEvent.change(screen.getByPlaceholderText(/topic/i), { target: { value: 'JavaScript' } });
     fireEvent.change(screen.getByPlaceholderText(/description/i), { target: { value: 'A group for JS lovers' } });
+    
     const createGroupButtonModal = screen.getByLabelText(/create-group-button-modal/i);
     fireEvent.click(createGroupButtonModal);
+    
     await waitFor(() => {
       expect(screen.getByText('New Group')).toBeInTheDocument();
       expect(screen.getByText('JavaScript')).toBeInTheDocument();
