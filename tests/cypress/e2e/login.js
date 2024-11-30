bb8f795f5e35e225cceeaff3ceb994d059b09fe1
keat <kavano.aresia@gmail.com>
2024-11-30 19:02:23 +0100

    added end-to-end tests

---
+++ tests/cypress/e2e/login.js
@@ -0,0 +1,20 @@
+describe('Login', () => {
+  it('should click the SignIn button', () => {
+    doLogin();
+    cy.contains(/Search/).should('be.visible');
+    cy.contains(/Dashboard/).should('be.visible');
+    cy.contains(/My Groups/).should('be.visible');
+    cy.contains(/Notifications/).should('be.visible');
+  })
+
+  it('should click the Connect Now button', () => {
+    cy.visit('https://alpha.unige-connect.it/');
+    cy.contains('button', 'CONNECT NOW').click();
+    cy.contains('button', 'Login').click();
+
+    cy.contains(/Search/).should('be.visible');
+    cy.contains(/Dashboard/).should('be.visible');
+    cy.contains(/My Groups/).should('be.visible');
+    cy.contains(/Notifications/).should('be.visible');
+  })
+})
