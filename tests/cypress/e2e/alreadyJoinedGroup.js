5922074ab46656df7d29194a92c91f476d1b66ad
samuele.romano <okko.sr@gmail.com>
2024-12-12 01:34:33 +0100

    first try e2e test

--- tests/cypress/e2e/alreadyJoinedGroup.js
+++ tests/cypress/e2e/alreadyJoinedGroup.js
@@ -4,12 +4,19 @@ describe('Already Joined a Group', () => {
     it('should show -Already a Member- in the card if the user already joined it', () => {
         doLogin();
         cy.contains('Groups').click();
-        cy.get('[aria-label="become-a-member-button"]').last().click();
+        cy.get('[aria-label="become-a-member-button"]').first().parent().as('group-card');
+        cy.get('@group-card').then(($el) => {
+            $el[0].style.border = '3px solid black'
+          });
+        cy.get('@group-card').within(() => {
+            cy.get('[aria-label="become-a-member-button"]').click();
+        });
         cy.get('[aria-label="become-a-member-modal-button"]').click();
         cy.get('[aria-label="message"]').should('exist');
-
-        cy.get('[aria-label="already-a-member"]').should('exist');
-
-
+        cy.get('@group-card').then(($el) => {
+            $el[0].style.border = '3px solid red'
+          });
+        cy.get('@group-card')
+        .find('[aria-label="already-a-member"]').should('exist');
     });
 });
