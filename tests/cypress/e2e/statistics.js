34d7445562a88533a8791f6c94a0dc3e942bb2c4
sanhijazi <h.hijazi711@gmail.com>
2025-01-05 22:21:00 +0100

    adding tests

---
+++ tests/cypress/e2e/statistics.js
@@ -0,0 +1,31 @@
+import { fillGroupModalFields, doProfLogin } from "./utils";
+
+describe('show statistics tab', () => {
+    it('should show 4 courses with digits', () => {
+        doProfLogin();
+        cy.contains('Statistics').click();
+
+        // Wait and verify the container is present
+        cy.get('.sc-dhGPYp', { timeout: 10000 }).should('exist');
+
+        // Verify the container has exactly 4 children
+        cy.get('.sc-dhGPYp').children().should('have.length', 3);
+
+        // Check that each child contains spans with the correct class and numeric text
+        cy.get('.sc-dhGPYp')
+            .children()
+            .each((child) => {
+                cy.wrap(child)
+                    .find('span')
+                    .each((span) => {
+                        cy.wrap(span).then(($span) => {
+                            if ($span.hasClass('sc-eePZqt')) {
+                                const text = $span.text().trim();
+                                const numericValue = Number(text);
+                                expect(numericValue).to.be.at.least(0);
+                            }
+                        });
+                    });
+            });
+    });
+});
